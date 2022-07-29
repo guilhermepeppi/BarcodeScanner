cdb0c34688da6ce64b86b5460b4f0cb1f2debc57
Guilherme Peppi <guilhermepeppi@gmail.com>
2022-07-29 12:14:54 -0300

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Guilherme Peppi on 29/07/22.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
